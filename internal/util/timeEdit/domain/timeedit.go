c38d14eb75bbe6b67f009ae6a0972a258a3dc416
Pieter Neyt <pieter.neyt@student.kdg.be>
2026-03-20 12:24:49 +0100

    agenda kleir

--- internal/util/timeEdit/domain/timeedit.go
+++ internal/util/timeEdit/domain/timeedit.go
@@ -2,6 +2,7 @@ package domain
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
@@ -15,12 +16,18 @@ type SearchObject struct {
 	ObjectID string `json:"objectId"`
 }
 
+type TypeField struct {
+	Type   string   `json:"type"`
+	Fields []string `json:"fields"`
+}
+
 type RequestBody struct {
 	Date          Date           `json:"date"`
 	IDFormat      string         `json:"idFormat"`
 	SearchObjects []SearchObject `json:"searchObjects"`
+	TypeFields    []TypeField    `json:"typeFields,omitempty"`
+	Statuses      []string       `json:"statuses,omitempty"`
 }
-
 type TypeID string
 
 const (
@@ -29,16 +36,33 @@ const (
 	Teaching        = "activitytype.teaching"
 )
 
+type FieldValue struct {
+	ID     string `json:"id"`
+	TypeID string `json:"typeId"`
+	Fields []struct {
+		FieldID string `json:"fieldId"`
+		Values  []struct {
+			Value string `json:"value"`
+		} `json:"values"`
+	} `json:"fields"`
+}
+
 type Object struct {
-	ObjectID string `json:"objectId"`
-	TypeID   string `json:"typeId"`
+	ObjectID   string       `json:"objectId"`
+	TypeID     string       `json:"typeId"`
+	FieldValue []FieldValue `json:"fieldValues,omitempty"`
 }
 
 type Result struct {
-	ID      int   `json:"id"`
-	Begin   int64 `json:"begin"`
-	End     int64 `json:"end"`
-	Objects []Object
+	ID      int      `json:"id"`
+	Begin   int64    `json:"begin"`
+	End     int64    `json:"end"`
+	Status  []string `json:"status"`
+	Objects []Object `json:"objects"`
+	Fields  []struct {
+		FieldID string   `json:"fieldId"`
+		Values  []string `json:"values"`
+	} `json:"fields"`
 }
 
 type ResponseBody struct {
@@ -48,11 +72,30 @@ type ResponseBody struct {
 	TotalResults int      `json:"totalResults"`
 	Results      []Result `json:"results"`
 }
+type CourseObjectsResponse struct {
+	Results []struct {
+		ExtID  string `json:"extId"`
+		Fields []struct {
+			FieldID string   `json:"fieldId"`
+			Values  []string `json:"values"`
+		} `json:"fields"`
+	} `json:"results"`
+}
+
+type AgendaItem struct {
+	ID         int      `json:"id"`
+	Begin      int64    `json:"begin"`
+	End        int64    `json:"end"`
+	Status     string   `json:"status"`
+	CourseName string   `json:"courseName"`
+	Rooms      []string `json:"rooms"`
+	Activity   string   `json:"activity"`
+	Attended   bool     `json:"attended"`
+}
 
 func CreateRequestBody(typeID TypeID, employeeId int) *RequestBody {
 	timeNow := time.Now().Unix()
-	// TODO dit gebruiken voor een valide les voor een andere les te gebruiken moet je een datum naar unix (int64) formaat zetten
-	//timeNow = 1773739800
+
 	return &RequestBody{
 		Date: Date{
 			StartDate: timeNow,
@@ -64,30 +107,109 @@ func CreateRequestBody(typeID TypeID, employeeId int) *RequestBody {
 				TypeID:   typeID,
 				ObjectID: fmt.Sprintf("person_%d", employeeId),
 			},
-			{
-				TypeID:   Teaching,
-				ObjectID: "_te_47675",
-			},
-			{
-				TypeID:   Teaching,
-				ObjectID: "_te_47676",
-			},
-			{
-				TypeID:   Teaching,
-				ObjectID: "_te_47677",
-			},
-			{
-				TypeID:   Teaching,
-				ObjectID: "_te_47678",
-			},
-			{
-				TypeID:   Teaching,
-				ObjectID: "_te_47679",
-			},
-			{
-				TypeID:   Teaching,
-				ObjectID: "_te_47680",
-			},
+			{TypeID: Teaching, ObjectID: "_te_47675"},
+			{TypeID: Teaching, ObjectID: "_te_47676"},
+			{TypeID: Teaching, ObjectID: "_te_47677"},
+			{TypeID: Teaching, ObjectID: "_te_47678"},
+			{TypeID: Teaching, ObjectID: "_te_47679"},
+			{TypeID: Teaching, ObjectID: "_te_47680"},
 		},
 	}
+}
+
+func CreateAgendaRequestBody(employeeId int, isStudent bool) *RequestBody {
+	typeID := TypeID(Lector)
+	if isStudent {
+		typeID = Student
+	}
+
+	now := time.Now()
+	startOfDay := time.Date(now.Year(), now.Month(), now.Day()-1, 0, 0, 0, 0, now.Location()).Unix()
+	endOfDay := time.Date(now.Year(), now.Month(), now.Day()-1, 23, 59, 59, 0, now.Location()).Unix()
+
+	return &RequestBody{
+		Date:     Date{StartDate: startOfDay, EndDate: endOfDay},
+		IDFormat: "EXTERNAL",
+		SearchObjects: []SearchObject{
+			{TypeID: typeID, ObjectID: fmt.Sprintf("person_%d", employeeId)},
+			{TypeID: Teaching, ObjectID: "_te_47675"},
+			{TypeID: Teaching, ObjectID: "_te_47676"},
+			{TypeID: Teaching, ObjectID: "_te_47677"},
+			{TypeID: Teaching, ObjectID: "_te_47678"},
+			{TypeID: Teaching, ObjectID: "_te_47679"},
+			{TypeID: Teaching, ObjectID: "_te_47680"},
+		},
+	}
+}
+
+func AgendaItemFromResult(r Result) AgendaItem {
+	item := AgendaItem{
+		ID:    r.ID,
+		Begin: r.Begin,
+		End:   r.End,
+	}
+
+	if len(r.Status) > 0 {
+		item.Status = r.Status[0]
+	}
+
+	for _, f := range r.Fields {
+		if f.FieldID == "r.title" && len(f.Values) > 0 && f.Values[0] != "" {
+			item.CourseName = f.Values[0]
+			break
+		}
+	}
+
+	for _, obj := range r.Objects {
+		switch obj.TypeID {
+		case "rooms":
+			item.Rooms = append(item.Rooms, strings.TrimPrefix(obj.ObjectID, "room_"))
+		case "activitytype.teaching":
+			item.Activity = obj.ObjectID
+		}
+	}
+
+	return item
+}
+
+func (item *AgendaItem) ResolveCourseFromObjects(objects []Object, courseNames map[string]string) {
+	if item.CourseName != "" {
+		return
+	}
+	for _, obj := range objects {
+		if obj.TypeID == "course" {
+			if name, ok := courseNames[obj.ObjectID]; ok {
+				item.CourseName = name
+			} else {
+				item.CourseName = obj.ObjectID
+			}
+			return
+		}
+	}
+}
+func (rb *ResponseBody) UniqueCourseIDs() []string {
+	seen := map[string]bool{}
+	var ids []string
+	for _, r := range rb.Results {
+		for _, obj := range r.Objects {
+			if obj.TypeID == "course" && !seen[obj.ObjectID] {
+				ids = append(ids, obj.ObjectID)
+				seen[obj.ObjectID] = true
+			}
+		}
+	}
+	return ids
+}
+
+func (r *CourseObjectsResponse) ToNameMap() map[string]string {
+	names := make(map[string]string, len(r.Results))
+	for _, obj := range r.Results {
+		for _, f := range obj.Fields {
+			if f.FieldID == "course.short.name" && len(f.Values) > 0 && f.Values[0] != "" {
+				names[obj.ExtID] = f.Values[0]
+				break
+			}
+		}
+	}
+	return names
 }
